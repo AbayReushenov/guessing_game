53e317163c9769a77b4338505a90a33153ce7211
AbayReushenov <abay.reushenov@gmail.com>
2025-09-01 15:24:06 +0300

    Угадай число.  rand::rng().random_range(1..101) генерирует случайнок целое число в диапазоне 1..100.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+extern crate rand;
+use rand::Rng;
 use std::io;
 fn main() {
     println!("Угадайте число!");
+    let secret_number = rand::rng().random_range(1..101);
+    println!("Загаданное число: {}", secret_number);
     println!("Пожалуйста, введите предположение.");
     let mut guess = String::new();
     io::stdin()
