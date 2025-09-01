a4bbce66a8c1bb420d8c60441af9907dd13db132
AbayReushenov <abay.reushenov@gmail.com>
2025-09-01 17:03:54 +0300

    Rust CLI game: Guessing game

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,36 @@ use std::io;
 fn main() {
     println!("Угадайте число!");
     let secret_number = rand::rng().random_range(1..101);
-    println!("Загаданное число: {}", secret_number);
-    println!("Пожалуйста, введите предположение.");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .ok()
-        .expect("Не удалось прочитать строку");
-    println!("Ваша попытка: {}", guess);
+    // println!("Загаданное число: {}", secret_number);
 
-    // Преобразуем ввод пользователя в число
-    let guess: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("Пожалуйста, введите число!");
-            return;
+    loop {
+        println!("Пожалуйста, введите предположение.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .ok()
+            .expect("Не удалось прочитать строку");
+        println!("Ваша попытка: {}", guess);
+
+        // Преобразуем ввод пользователя в число
+        // Оно может иметь тип i32 — 32-битное целое, или u32 — 32-битное целое без знака,
+        // или i64 — 64-битное целое, или какой-нибудь другой. По умолчанию, Rust сделает его
+        // 32-битным целым, i32.
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        // Метод cmp() может быть вызван у чего-либо, что может сравниваться, и получает
+        // ссылку на то, с чем мы хотим его сравнить. Результатом сравнения будет тип Ordering ,
+        // который мы добавили выше. Мы используем оператор match для определения Ordering —
+        // результата сравнения. Ordering — перечисление.
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Слишком маленькое!"),
+            Ordering::Greater => println!("Слишком большое!"),
+            Ordering::Equal => {
+                println!("Вы выиграли!");
+                break;
+            }
         }
-    };
-    // Метод cmp() может быть вызван у чего-либо, что может сравниваться, и получает
-    // ссылку на то, с чем мы хотим его сравнить. Результатом сравнения будет тип Ordering ,
-    // который мы добавили выше. Мы используем оператор match для определения Ordering —
-    // результата сравнения. Ordering — перечисление.
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Слишком маленькое!"),
-        Ordering::Greater => println!("Слишком большое!"),
-        Ordering::Equal => println!("Вы выиграли!"),
     }
 }
